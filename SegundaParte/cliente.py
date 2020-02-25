3f733f189bdd0efcce1cea4ca681bf4299c86f92
Edwin-En <53127254+Edwin-En@users.noreply.github.com>
2020-02-25 13:34:57 -0500

    Add files via upload

---
+++ SegundaParte/cliente.py
@@ -0,0 +1,60 @@
+# Cliente que pide conexion a un servidor local para hacer uso de una calculadora
+
+import zmq
+import os
+import json
+
+
+def Enviar():
+
+    archivo = open("prueba.txt" , "rb")
+    contenido = archivo.read(1024)
+    while contenido:                               # Recibe por teclado y guarda en la variable "x"
+        socket.send(contenido)                           # hace el send (envia) envia un mensaje
+        contenido = archivo.read(1024)                 # recibe del servidor un mensaje si no lo recibiera para la ejecucion
+
+    archivo.close()
+    print("El archivo se ha enviado")
+    mensajeentrante = socket.recv_string()
+
+def Recibir():
+    archivorecibido = open("archivorecibidocliente", "wb")
+    socket.send_string("ok")
+    data = socket.recv(1024)
+    archivorecibido.write(data)
+
+    print("Se ha recibido el archivo")
+    archivorecibido.close()
+    socket.send_string("Ok")
+    socket.recv_string()
+
+def Listar():
+    socket.send_string("ok")
+    lista = socket.recv_string()
+    listajson = json.loads(lista)
+    print("se muestra la lista de archivos")
+    print(listajson)
+
+
+if __name__ == "__main__":
+    context = zmq.Context()
+    print("Conectando con el servidor")
+    socket = context.socket(zmq.REQ)                # REQ define el rol del cliente (hacer solicitudes)
+    socket.connect("tcp://localhost:5555")
+    print("Se encuentra conectado al servidor ")            # Imprime un mensaje
+    condicion = True
+    while condicion:
+        print("¿Que operacion desea realizar?")
+        print("1. Enviar 2. Recibir 3. Listar 4. Salir")
+        opcion = input()
+        socket.send_string(opcion)
+        mensajeentra = socket.recv_string()
+        if opcion == "1":
+            Enviar()
+        if opcion == "2":
+            Recibir()
+        if opcion == "3":
+            Listar()
+        if opcion == "4":
+            socket.close()
+            condicion = False
---
+++ SegundaParte/servidor.py
@@ -0,0 +1,56 @@
+# Servidor que presta el servicio de una calculadora
+
+import zmq
+import os
+import json
+
+
+def Recibir():
+    #if os.path.exists("CarpetaServidor"):
+    #carpeta = os.mkdir("CarpetaServidor")
+    archivorecibido = open("/home/utp/Descargas/arq/CarpetaServidor/archivorecibido", "wb")
+    data = socket.recv(1024)
+    archivorecibido.write(data)
+
+    print("Se ha recibido el archivo")
+    archivorecibido.close()
+    socket.send_string("Ok")
+    #else:
+    #    print("error al recibir el archivo")
+
+
+def Enviar():
+
+
+    archivo = open("prueba.txt" , "rb")
+    contenido = archivo.read(1024)
+    socket.recv_string()
+    while contenido:                               # Recibe por teclado y guarda en la variable "x"
+        socket.send(contenido)                           # hace el send (envia) envia un mensaje
+        contenido = archivo.read(1024)                 # recibe del servidor un mensaje si no lo recibiera para la ejecucion
+
+    archivo.close()
+    print("El archivo se ha enviado")
+    mensajeentrante = socket.recv_string()
+    socket.send_string("ok")
+
+def Listar():
+    socket.recv_string()
+    lista = os.listdir("/home/utp/Descargas/arq/CarpetaServidor")                  # Ruta especifica de la carpeta que desea listar
+    listajson = json.dumps(lista)
+    socket.send_string(listajson)
+
+if __name__ == "__main__":
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+    while True:
+        print("Servidor activo")
+        mensaje = socket.recv_string()
+        mensajesale = socket.send_string("ok")
+        if mensaje == "1":
+            Recibir()
+        if mensaje == "2":
+            Enviar()
+        if mensaje == "3":
+            Listar()
